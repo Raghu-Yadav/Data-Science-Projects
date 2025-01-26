eb92cdbf35641467b3b731cace4807e3aea04df3
Raghu-Yadav <raghukolusu333@gmail.com>
2025-01-26 21:05:24 +0530

    data transformation done

--- Student Performace Prediction/src/components/data_ingestion.py
+++ Student Performace Prediction/src/components/data_ingestion.py
@@ -6,6 +6,9 @@ from src.logger import logging
 import sys
 import os
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 
 @dataclass
 class DataIngestionConfig:
@@ -56,4 +59,7 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
